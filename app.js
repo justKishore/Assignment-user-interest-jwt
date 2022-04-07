d298edd83abe9a40205e263d0a52b0b112693cb3
Kishore Sudhir <kishore.sudhir@cesltd.com>
2022-04-07 14:22:46 +0530

    3.Create user model and route

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 // S2 Create Nodejs Server and connect to database
 
+// S3 Create user model and route
+
 require("dotenv").config(); // S2
 
 require("./config/database").connect(); // S2
 const { application } = require("express"); // S2
 const express = require("express"); // S2
+const User = require("./model/user"); // S3
 
 const app = express(); // S2
 
 app.use(express.json()); // S2
+
+// S3
+
+// Home
+app.get("/", (req, res) => {
+  res.send("HI");
+});
+
+// Sign UP
+app.post("/signup", (res, req) => {});
 
 module.exports = app;
--- model/user.js
+++ model/user.js
@@ -1,1 +1,13 @@
-// user.js
+// S3 Create user model and route
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fname: String,
+  interest: Number,
+  email: { type: String, unique: true },
+  password: String,
+  token: String,
+});
+
+module.exports = mongoose.model("user", userSchema);
